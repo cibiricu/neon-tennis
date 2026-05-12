79411841c243a9c879831c5ec88b8a149d5e08b9
cibiricu <cibiricu@hotmail.com>
2026-05-12 10:58:01 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'neon-tennis-v1';
+const FILES = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
