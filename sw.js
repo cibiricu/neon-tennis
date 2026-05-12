3f743afa15d0132244471c352788103f1dc0bf11
cibiricu <cibiricu@hotmail.com>
2026-05-12 13:17:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,18 @@
-const CACHE = 'neon-tennis-v1';
-const FILES = ['index.html', 'manifest.json'];
+const CACHE = 'neon-tennis-v2';
+const FILES = ['index.html', 'manifest.json', 'icon.png', 'icon-192.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{}))
+  );
   self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+  ));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
