b7469f325760f38a6e33f863a9457ab3a27503cf
cibiricu <cibiricu@hotmail.com>
2026-05-13 16:54:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
-const CACHE_NAME = 'neon-tennis-rush-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
+const CACHE = 'neon-tennis-v2';
+const FILES = ['index.html', 'manifest.json', 'icon.png', 'icon-192.png'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{}))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
